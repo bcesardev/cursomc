518942565fa70af722eded41a1b81402dabad1c5
Bruno Cesar <bcesar.bruno@gmail.com>
2018-07-23 15:31:56 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/cesar/bruno/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.cesar.bruno.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cesar.bruno.cursomc.domain.Pedido;
+import com.cesar.bruno.cursomc.repositories.PedidoRepository;
+import com.cesar.bruno.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName());
+		}
+		return obj;
+	}
+
+}
