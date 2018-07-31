808f96e3c6e328ee5c2d83234f9ad3a0f6338eaf
Bruno Cesar <bcesar.bruno@gmail.com>
2018-07-31 15:12:32 -0300

    Validacao sintática com Bean validation

--- src/main/java/com/cesar/bruno/cursomc/services/CategoriaService.java
+++ src/main/java/com/cesar/bruno/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.cesar.bruno.cursomc.domain.Categoria;
+import com.cesar.bruno.cursomc.dto.CategoriaDTO;
 import com.cesar.bruno.cursomc.repositories.CategoriaRepository;
 import com.cesar.bruno.cursomc.services.exceptions.DataIntegrityException;
 import com.cesar.bruno.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,5 +56,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
