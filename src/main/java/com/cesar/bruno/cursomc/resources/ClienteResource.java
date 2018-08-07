2652ed93c7693e16da932186491fc4b10d0dca5d
Bruno Cesar <bcesar.bruno@gmail.com>
2018-08-07 16:22:00 -0300

    Criando profile de teste

--- src/main/java/com/cesar/bruno/cursomc/resources/ClienteResource.java
+++ src/main/java/com/cesar/bruno/cursomc/resources/ClienteResource.java
@@ -20,9 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.cesar.bruno.cursomc.domain.Categoria;
 import com.cesar.bruno.cursomc.domain.Cliente;
-import com.cesar.bruno.cursomc.dto.CategoriaDTO;
 import com.cesar.bruno.cursomc.dto.ClienteDTO;
 import com.cesar.bruno.cursomc.dto.ClienteNewDTO;
 import com.cesar.bruno.cursomc.services.ClienteService;
