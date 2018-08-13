abd3c9d23d1e311067297593aef533f81b192446
Bruno Cesar <bcesar.bruno@gmail.com>
2018-08-13 12:11:39 -0300

    Email HTML

--- src/main/java/com/cesar/bruno/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/cesar/bruno/cursomc/services/SmtpEmailService.java
@@ -1,9 +1,12 @@
 package com.cesar.bruno.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractEmailService {
 
@@ -12,10 +15,20 @@ public class SmtpEmailService extends AbstractEmailService {
 	@Autowired
 	private MailSender mailSender;
 
+	@Autowired
+	private JavaMailSender javaMailSender;
+
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 		log.info("Enviando email...");
 		mailSender.send(msg);
+		log.info("Email enviado");
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		log.info("Enviando email...");
+		javaMailSender.send(msg);
 		log.info("Email enviado");
 	}
 
