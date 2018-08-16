9bb4fa5598c45456179dce91707b22116ce64bf7
Bruno Cesar <bcesar.bruno@gmail.com>
2018-08-16 14:22:55 -0300

    Implementando autenticacao e geracao do token JWT

--- src/main/java/com/cesar/bruno/cursomc/services/DBService.java
+++ src/main/java/com/cesar/bruno/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.cesar.bruno.cursomc.domain.PagamentoComCartao;
 import com.cesar.bruno.cursomc.domain.Pedido;
 import com.cesar.bruno.cursomc.domain.Produto;
 import com.cesar.bruno.cursomc.domain.enums.EstadoPagamento;
+import com.cesar.bruno.cursomc.domain.enums.Perfil;
 import com.cesar.bruno.cursomc.domain.enums.TipoCliente;
 import com.cesar.bruno.cursomc.repositories.CategoriaRepository;
 import com.cesar.bruno.cursomc.repositories.CidadeRepository;
@@ -114,16 +115,21 @@ public class DBService {
 		cidadeRepository.save(Arrays.asList(c1, c2, c3));
 		
 		Cliente cli1 = new Cliente(null, "Maria Silva", "bcesar.bruno@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
-		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
+		Cliente cli2 = new Cliente(null, "Ana Costas", "bruno.depaula@diadeis.com", "91053040679", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.getTelefones().addAll(Arrays.asList("955552222", "43211555"));
+		cli2.addPerfil(Perfil.ADMIN);		
+
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2106", null, "Centro", "06417000", cli2, c2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 		
-		clienteRepository.save(Arrays.asList(cli1));
-		enderecoRepository.save(Arrays.asList(e1, e2));
+		clienteRepository.save(Arrays.asList(cli1, cli2));
+		enderecoRepository.save(Arrays.asList(e1, e2, e3));
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
 		
